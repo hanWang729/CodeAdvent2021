475b2bb16187ce40acf06003d5448e35d95282d5
hanWang729 <wanghanapply@163.com>
2022-12-20 09:44:13 +0100

    18-2 draft

--- day18-2.cpp
+++ day18-2.cpp
@@ -12,6 +12,12 @@
 int MOD = 100;
 
 int hash(int x, int y, int z){
+    if(x == -1)
+        x = 99;
+    if(y == -1)
+        y = 99;
+    if(z == -1)
+        z = 99;
     return x * MOD * MOD + y * MOD + z;
 }
 
@@ -55,6 +61,7 @@ struct Lava
     int max_z;
     std::set<int> occupied_hash_set;
     std::set<int> lava_hash_set;
+    std::set<int> air_hash_set;
 
     Lava(){
         sides = 0;
@@ -75,41 +82,88 @@ struct Lava
         occupied_hash_set.insert(hash(new_cube.x, new_cube.y, new_cube.z));
         lava_hash_set.insert(hash(new_cube.x, new_cube.y, new_cube.z));
         if(max_x < new_cube.x)
-            max_x == new_cube.x;
+            max_x = new_cube.x;
         if(max_y < new_cube.y)
             max_y = new_cube.y;
         if(max_z < new_cube.z)
             max_z = new_cube.z;
     }
 
-    void removeAir(){
-        for(int x = 0; x <= max_x; x++){
-            for(int y = 0; y <= max_y; y++){
-                for(int z = 0; z <= max_z; z++){
-                    if(occupied_hash_set.count(hash(x,y,z)) == 0){
-                        if(isAir(x,y,z)){
-                            int s = expandAir(x,y,z);
-                            sides -= s;
-                        }
-                    }
-                    
+    void findAir(){
+        for(int x = -1; x <= max_x + 1; x++){
+            for(int y = -1; y <= max_y + 1; y++){
+                for(int z = -1; z <= max_z + 1; z++){
+                    if(lava_hash_set.count(hash(x,y,z)) == 0)
+                        air_hash_set.insert(hash(x,y,z));
                 }
             }
         }
     }
 
-    bool isAir(int x, int y, int z){
-        // int h = hash(x,y,z);
-        int max_x, max_y, max_z;
-        int min_x, min_y, min_z;
-        for(auto i : lava_hash_set){
-            
-        }
-
+    void removeAir1(int x, int y, int z){
+        if(air_hash_set.count(hash(x+1,y,z)))
+            removeAir1(x+1,y,z);
+        if(air_hash_set.count(hash(x,y+1,z)))
+            removeAir1(x,y+1,z);
+        if(air_hash_set.count(hash(x,y,z+1)))
+            removeAir1(x,y,z+1);
+        if(air_hash_set.count(hash(x,y,z)))
+            air_hash_set.erase(hash(x,y,z));
+        if(air_hash_set.count(hash(x-1,y,z)))
+            removeAir1(x-1,y,z);
+        if(air_hash_set.count(hash(x,y-1,z)))
+            removeAir1(x,y-1,z);
+        if(air_hash_set.count(hash(x,y,z-1)))
+            removeAir1(x,y,z-1);
+        
     }
 
-    int expandAir(int x, int y, int z){
+    void removeAir2(int x, int y, int z){
+        // if(air_hash_set.count(hash(x+1,y,z)))
+        //     removeAir(x+1,y,z);
+        // if(air_hash_set.count(hash(x,y+1,z)))
+        //     removeAir(x,y+1,z);
+        // if(air_hash_set.count(hash(x,y,z+1)))
+        //     removeAir(x,y,z+1);
+        if(air_hash_set.count(hash(x-1,y,z)))
+            removeAir2(x-1,y,z);
+        if(air_hash_set.count(hash(x,y-1,z)))
+            removeAir2(x,y-1,z);
+        if(air_hash_set.count(hash(x,y,z-1)))
+            removeAir2(x,y,z-1);
+        if(air_hash_set.count(hash(x,y,z)))
+            air_hash_set.erase(hash(x,y,z));
+        
+    }
 
+    // void removeAir(int x, int y, int z, std::set<int>& tmp_hash_set){
+    //     if(tmp_hash_set.count(hash(x,y,z)) == 0)
+    //         tmp_hash_set.insert(hash(x,y,z));
+    //     if(air_hash_set.count(hash(x+1,y,z)))
+    //         removeAir(x+1,y,z);
+    //     if(air_hash_set.count(hash(x-1,y,z)))
+    //         removeAir(x-1,y,z);
+    //     if(air_hash_set.count(hash(x,y+1,z)))
+    //         removeAir(x,y+1,z);
+    //     if(air_hash_set.count(hash(x,y-1,z)))
+    //         removeAir(x,y-1,z);
+    //     if(air_hash_set.count(hash(x,y,z+1)))
+    //         removeAir(x,y,z+1);
+    //     if(air_hash_set.count(hash(x,y,z-1)))
+    //         removeAir(x,y,z-1);
+        
+    // }
+
+    void removeAir_helper(){
+        std::set<int> tmp_hash_set;
+        removeAir1(0,0,0);
+        removeAir1(max_x,max_y,max_z);
+    }
+
+    void print_max(){
+        std::cout<< "x_max: " << max_x << std::endl;
+        std::cout<< "y_max: " << max_y << std::endl;
+        std::cout<< "z_max: " << max_z << std::endl;
     }
 };
 
@@ -137,5 +191,25 @@ Lava readInput()
 
 int main(int argc, char* argv[]){
     auto lava = readInput();
-    std::cout << "ans1: " << lava.sides << std::endl;
+    // std::cout << "ans1: " << lava.sides << std::endl;
+    lava.findAir();
+    lava.print_max();
+    // for(auto i : lava.air_hash_set){
+    //     std::cout << i << std::endl;
+    // }
+    std::cout << lava.air_hash_set.size() << std::endl;
+    lava.removeAir_helper();
+    // for(auto i : lava.air_hash_set){
+    //     std::cout << i << std::endl;
+    // }
+    std::cout << lava.air_hash_set.size() << std::endl;
+    Lava airLava;
+    for(auto ahs : lava.air_hash_set){
+        int x = ahs / (MOD * MOD);
+        int y = (ahs - x * MOD * MOD) / MOD;
+        int z = ahs % (MOD * MOD);
+        Cube cube = Cube(x,y,z);
+        airLava.addCube(cube);
+    }
+    std::cout << "ans2: " << lava.sides - airLava.sides << std::endl;
 }
